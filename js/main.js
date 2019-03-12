abdd7672f293b24ea63703ce26292b14c3e8a102
Gor Melqonyan <gormelqonyan1997@gmail.com>
2019-03-12 21:05:01 +0400

    select btn update

--- js/main.js
+++ js/main.js
@@ -1,44 +1,64 @@
 $(function(){
-  var paralax1 = $('.paralax1')
-  var paralax2 = $('.paralax2')
-  var paralax3 = $('.paralax3')
-  var paralax4 = $('.paralax4')
-  var paralax5 = $('.paralax5')
-  var paralax6 = $('.paralax6')
-  var paralax7 = $('.paralax7')
-  var paralaxfon2 = $('.paralaxfon2')
+  var paralax = {
+    paralax1 : $('.paralax1'),
+    paralax2 : $('.paralax2'),
+    paralax3 : $('.paralax3'),
+    paralax4 : $('.paralax4'),
+    paralax5 : $('.paralax5'),
+    paralax6 : $('.paralax6'),
+    paralax7 : $('.paralax7'),
+    paralaxfon2 : $('.paralaxfon2'),
+  }
 
   $(document).mousemove(function(e){
-    var valueX = (e.pageX * -1/15);
-    var valueY= (e.pageY * -1/15);
-    var valueX2 = (e.pageX * -1/30);
-    var valueY2= (e.pageY * -1/30);
-    var valueX3 = (e.pageX * -1/8);
-    var valueY3= (e.pageY * -1/8);
 
-    paralax1.css({
-      'transform':'translate3d('+valueX+'px, '+valueY+'px, 0)'
+    var position = {
+      valueX : (e.pageX * -1/15),
+      valueY : (e.pageY * -1/15),
+      valueX2 : (e.pageX * -1/30),
+      valueY2 : (e.pageY * -1/30),
+      valueX3 : (e.pageX * -1/8),
+      valueY3 : (e.pageY * -1/8),
+    }
+
+    paralax.paralax1.css({
+      'transform':'translate3d('+position.valueX+'px, '+position.valueY+'px, 0)'
     })
-    paralax2.css({
-      'transform':'translate3d('+(valueX2*-1)+'px, '+(valueX2*-1)+'px, 0)'
+    paralax.paralax2.css({
+      'transform':'translate3d('+(position.valueX2*-1)+'px, '+(position.valueX2*-1)+'px, 0)'
     })
-    paralax3.css({
-      'transform':'translate3d('+(valueX/2)+'px, '+(valueY/2)+'px, 0)'
+    paralax.paralax3.css({
+      'transform':'translate3d('+(position.valueX/2)+'px, '+(position.valueY/2)+'px, 0)'
     })
-    paralax4.css({
-      'transform':'translate3d('+(valueX3*-1)+'px, '+(valueY3*-1)+'px, 0)'
+    paralax.paralax4.css({
+      'transform':'translate3d('+(position.valueX3*-1)+'px, '+(position.valueY3*-1)+'px, 0)'
     })
-    paralax5.css({
-      'transform':'translate3d('+(valueX3/3)+'px, '+(valueY3/3)+'px, 0)'
+    paralax.paralax5.css({
+      'transform':'translate3d('+(position.valueX3/3)+'px, '+(position.valueY3/3)+'px, 0)'
     })
-    paralax6.css({
-      'transform':'translate3d('+(valueX3/-4)+'px, '+(valueY3/-4)+'px, 0)'
+    paralax.paralax6.css({
+      'transform':'translate3d('+(position.valueX3/-4)+'px, '+(position.valueY3/-4)+'px, 0)'
     })
-    paralax7.css({
-      'transform':'translate3d('+(valueX3/-4)+'px, '+(valueY3/-4)+'px, 0)'
+    paralax.paralax7.css({
+      'transform':'translate3d('+(position.valueX3/-4)+'px, '+(position.valueY3/-4)+'px, 0)'
     })
-    paralaxfon2.css({
-      'transform':'translate3d('+(valueX2/-6)+'px, '+(valueY2/-6)+'px, 0)'
+    paralax.paralaxfon2.css({
+      'transform':'translate3d('+(position.valueX2/-6)+'px, '+(position.valueY2/-6)+'px, 0)'
     })
   })
+
+  if ($('.select-options ul li').hasClass('selected')) {
+    $('.select .input').text($('.select-options li.selected > span:first-child').text());
+    $('.select-hidden').val($('.select-options li.selected > span:first-child').text())
+  }
+
+  $('.select').click(function() {
+    $('.select-options').toggleClass('visible');
+  });
+  $('.select-options li').click(function() {
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('.select .input').text($(this).find('span:first-child').text());
+    $('.select-hidden').val($(this).find('span:first-child').text())
+  });
 })
